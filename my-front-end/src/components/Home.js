8b51526684b2171c83989f5134335f261b6c3029
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-01-27 20:00:57 -0500

    edited Home

--- my-front-end/src/components/Home.js
+++ my-front-end/src/components/Home.js
@@ -7,9 +7,9 @@ function Home () {
             <br></br>
             <h1>🎨 Artist's Library 🎨</h1>
             {/* <hr/> */}
-                <p> This application allows you to have a virtual art collection.</p>
+                <p> This application allows you to have a virtual art collection for free.</p>
                 <p>You can also add an artist to your collection and add and delete paintings from an artist.</p>
-                <p>Click on a route to get started!</p>
+                <p>Click on a tab to get started!</p>
         </div>
     )
 
